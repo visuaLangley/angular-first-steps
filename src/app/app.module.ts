65f61d6c96dc662bbc4cecb31e5d84ad91ed012a
visuaLangley <millarpjorge@gmail.com>
2024-04-25 17:47:04 -0400

    fin S-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from "./counter/counter.components";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CounterModule,
+        HeroesModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: `
+            <hr>
+            <h3>Counter: {{counter}} </h3>
+            <button (click)="increaseBy(+1)">+1</button>
+            <button (click)="reset(10)"> Reset</button>
+            <button (click)="decreaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent{
+  counter : number = 10;
+  increaseBy(value : number) : void{
+    this.counter += 1;
+    console.log(9+'adssdasdasd')
+  }
+  reset(value : number) : void{
+    this.counter = 10;
+  }
+  decreaseBy(value : number): void{
+    this.counter -= 1;
+    if(this.counter < 0){
+      this.counter = 0;
+    }
+  }
+
+}
